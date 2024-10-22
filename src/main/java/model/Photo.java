8ed22c2132013dd0e78b6a32efd5908a85e2b669
Tanțoș Alexandra-Bianca <114863064+AlexandraTantos@users.noreply.github.com>
2024-10-22 22:22:33 +0300

    add nested classes and abstract class

--- src/main/java/model/Photo.java
+++ src/main/java/model/Photo.java
@@ -1,12 +1,38 @@
 package model;
 
 public class Photo extends MediaContent {
-    public Photo(String filePath, String description) {
-        super(filePath, description);
+    private String resolution;
+
+    public Photo(String title, String description, String resolution) {
+        super(title, description);
+        this.resolution = resolution;
     }
 
     @Override
-    public void displayContent() {
-        System.out.println("Displaying photo: " + getDescription());
+    public void display() {
+        System.out.println("Displaying photo: " + title + " with resolution: " + resolution);
+    }
+
+    public String getResolution() {
+        return resolution;
+    }
+
+    public void setResolution(String resolution) {
+        this.resolution = resolution;
+    }
+
+    @Override
+    public String toString() {
+        return "Photo{" + "resolution='" + resolution + '\'' + "} " + super.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equals(o)) return false;
+        Photo photo = (Photo) o;
+        return resolution.equals(photo.resolution);
     }
 }
+
